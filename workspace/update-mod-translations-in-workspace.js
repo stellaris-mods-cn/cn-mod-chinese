0187cc6ccee0d0fce803161f7621b2e2867c877a
tommyZZM <452490446@qq.com>
2019-02-27 09:40:30 +0800

    update: 更新目录结构

--- workspace/update-mod-translations-in-workspace.js
+++ workspace/update-mod-translations-in-workspace.js
@@ -3,10 +3,12 @@ const path = require('path')
 const fs = require('mz/fs')
 const vfs = require('vinyl-fs')
 const through = require('through2')
+const convertEncoding = require('gulp-convert-encoding');
 // const rename = require('gulp-rename')
 const argv = require('yargs')
   .usage('$0 -t <target_mod_name>')
   .alias('t', 'target')
+  // .alias('r', 'relativePath')
   .argv;
 
 const { target } = argv;
@@ -25,40 +27,70 @@ function syParseAsObject(content) {
 
 function syReplaceKeyValuePair(content, replacer) {
   const contentStr = content.toString();
-  return contentStr.replace(/([\w\.]+):(\d)+\s+"(.+)"/gi, function (_, key, d, value) {
-    return `${key}:${d} "${replacer(key, value)}"`;
+  return contentStr.replace(/([\w\.]+)(:\d*)?\s+"(.+)"/gi, function (_, key, suffix, value) {
+    return `${key}${suffix} "${replacer(key, value)}"`;
   });
 }
 
+const dirSourceLocalisation = path.join(dirSource, target, 'localisation');
+
 vfs.src(
-    path.join(dirSource, target, 'localisation/*_english.yml')
+    path.join(dirSourceLocalisation, '*/*_english.yml')
   )
   .pipe(through.obj(async (file, _, next) => {
     // 合并最新的mod文本文件和已经翻译的文本文件.
     const filepath = file.path;
     const filedir = path.dirname(filepath);
     const basename = path.basename(filepath);
+    const relativePathFromLocalisation = path.dirname(path.relative(dirSourceLocalisation, filepath));
+
+    const relativePathFromLocalisationArr = relativePathFromLocalisation.split('/');
+
+    const isScoped = relativePathFromLocalisationArr[0] === 'english';
 
     const basenameChinese = basename.replace(/_english\.yml$/gi, '_simp_chinese.yml');
 
-    const filepathTranlated = path.join(dirTranlated, 'localisation', basenameChinese);
+    const filepathTranslated = path.join(
+      dirTranlated, 'localisation', basenameChinese
+    );
 
-    const isExists = await fs.exists(filepathTranlated);
+    const filepathScopedTranslated = path.join(
+      dirTranlated, 'localisation/simp_chinese', basenameChinese
+    );
 
-    if (!isExists) {
+    const isScopedExists = await fs.exists(filepathScopedTranslated);
+
+    const isExists = await fs.exists(filepathTranslated);
+
+    if (!isScopedExists && !isExists) {
       // 如果是新文件，则直接重命名复制
       return next();
     }
 
-    const currentContent = await fs.readFile(filepathTranlated);
-    const keyMap = syParseAsObject(currentContent);
+    const lastTranslatedContent = await fs.readFile(
+      isScopedExists ? filepathScopedTranslated : filepathTranslated
+    );
+
+    const keyMap = syParseAsObject(lastTranslatedContent);
     const newContent = syReplaceKeyValuePair(file.contents, (key, value) => {
       return keyMap[key] || value;
     }).replace(/^l_english/, 'l_simp_chinese');
+
+    let newRelativePathFromLocalisation = ''
+    if (isScoped) {
+      const dirs = [...relativePathFromLocalisationArr];
+      dirs[0] = 'simp_chinese';
+      newRelativePathFromLocalisation = dirs.join('/');
+    }
     
     file.contents = Buffer.from(newContent);
-    file.path = path.join(filedir, basenameChinese);
-    next(null, file);
+    file.path = path.join(
+      dirSourceLocalisation, newRelativePathFromLocalisation, basenameChinese
+    );
+    // console.log(newRelativePathFromLocalisation)
+    next(null, file); 
+    // next();
   }))
+  .pipe(convertEncoding({to: 'utf8', iconv: { stripBOM: false, addBOM: true }}))
   .pipe(vfs.dest(path.join(dirDest, target, 'localisation')))
 
